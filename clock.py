7a88fb1f0e53c396064ba2525a35ee86f12c2fa4
e-mellinandrichier <emmanuelle.mellinand-richier@laplateforme.io>
2025-01-07 15:51:34 +0100

    12/24 added everywhere except on the alarm

--- clock.py
+++ clock.py
@@ -1,5 +1,5 @@
 import datetime
-
+import os
 import time
 
 def current_time():
@@ -8,26 +8,49 @@ def current_time():
         my_datetime = datetime.datetime.today()
 
         my_time = my_datetime.time()
-
-        c_time = my_time.isoformat(timespec="seconds")
+        if twelveortwentyfour == "24" : 
+            c_time = my_time.strftime("%H:%M:%S")
+        if twelveortwentyfour == "12" :
+            c_time = my_time.strftime("%I:%M:%S%p")
+        os.system('cls')
         print(c_time)
         if answer1 == "oui":
             alarm(alarm_hour, alarm_minute, alarm_second, my_time.hour, my_time.minute, my_time.second)
         time.sleep(1)
 
 def display_time(a, b, c):
+    d = a
     while 1 == 1:
+        amorpm = ""
         if c == 60:
             b +=1
             c = 0
         if b == 60:
             a += 1
+            d += 1
             b = 0
-        if a == 24:
-            a = 0
+        if d == 24:
+            d = 0
+        if twelveortwentyfour == "24":
+            if a == 24:
+                a = 0
+        if twelveortwentyfour == "12":
+            if a > 12:
+                a -= 12
+            if a == 13:
+                a = 1
+            if d <= 11:
+                amorpm = "AM"
+            if d > 11:
+                amorpm = "PM"
         timer = datetime.time(a, b, c)
-        print(timer)
+        os.system('cls')
+        print(timer, amorpm)
+
         if answer1 == "oui":
+            if twelveortwentyfour == "12":
+                if amorpm == "PM":
+                    a +=12
             alarm(alarm_hour, alarm_minute, alarm_second, a, b, c)
         c +=1
         time.sleep(1)
@@ -39,7 +62,8 @@ def alarm(a_h, a_m, a_s, c_h, c_m, c_s):
                 print("it's tiiiiime") 
 
 print("Bonjour Mamie ! Que veux tu faire aujourd'hui ?")
-answer = input(" Veux-tu afficher l'heure actuelle ? (oui/non)")
+twelveortwentyfour = input("Veux-tu mettre le temps en 24h ou en 12h ?(24/12)")
+answer = input("Veux-tu afficher l'heure actuelle ? (oui/non)")
 if answer == "oui":
     answer1 = input("Veux tu régler une alarme ? (oui/non)")
     if answer1 == "oui":
@@ -61,10 +85,4 @@ if answer == "non":
             alarm_second = int(input("second :"))
         display_time(display_time_hour, display_time_minute, display_time_second)
     if answer2 == "non" :
-            print("bonne journée mamie !")
-*
-# a = 11
-# b = 22
-# c = 33
-# x = datetime.now()
-# truc = x.replace(hour = a, minute = b, second = c)
+        print("bonne journée mamie !")
